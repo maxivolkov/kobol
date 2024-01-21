93d4f0a41ad55e1a7e067b9d50e4344c693843ae
Maksim <maksim.volkov@mail.com>
2024-01-21 18:41:02 +0100

    Update main.h

--- src/main.h
+++ src/main.h
@@ -71,5 +71,44 @@ enum rank : int
 	rank_8
 };
 
+
+enum direction : int
+{
+	north = 8,
+	northeast = 9,
+	east = 1,
+	southeast = -7,
+	south = -8,
+	southwest = -9,
+	west = -1,
+	northwest = 7,
+	northnorth = 16,
+	southsouth = -16
+};
+
+constexpr size_t nsquares = 64;
+constexpr size_t ncolors = 2;
+constexpr size_t ndirs = 8;
+constexpr size_t npiece_types = 6;
+const std::string piece_str = "PNBRQK~>pnbrqk.";
 const std::string start_fen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq -";
 
+inline const std::string sqstr[65] = {
+  "a1", "b1", "c1", "d1", "e1", "f1", "g1", "h1",
+  "a2", "b2", "c2", "d2", "e2", "f2", "g2", "h2",
+  "a3", "b3", "c3", "d3", "e3", "f3", "g3", "h3",
+  "a4", "b4", "c4", "d4", "e4", "f4", "g4", "h4",
+  "a5", "b5", "c5", "d5", "e5", "f5", "g5", "h5",
+  "a6", "b6", "c6", "d6", "e6", "f6", "g6", "h6",
+  "a7", "b7", "c7", "d7", "e7", "f7", "g7", "h7",
+  "a8", "b8", "c8", "d8", "e8", "f8", "g8", "h8",
+  "None"
+};
+
+inline const std::string move_typestr_uci[8] = {
+  "", "", "", "", "n", "b", "r", "q"
+};
+
+inline const char* move_typestr[16] = {
+  "", "", " O-O", " O-O-O", "N", "B", "R", "Q", " (capture)", "", " e.p.", "", "N", "B", "R", "Q"
+};
