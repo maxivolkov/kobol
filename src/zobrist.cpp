4f153bda79245e2a5841fcd410c023fd092f6e71
Maksim <maksim.volkov@mail.com>
2024-01-21 20:21:26 +0100

    Create zobrist.cpp

---
+++ src/zobrist.cpp
@@ -0,0 +1,10 @@
+#include "position.h"
+#include "zobrist.h"
+
+void init_zobrist()
+{
+	hash_color = prng::rand64();
+	for (auto& i : zobrist_table)
+		for (unsigned long long& j : i)
+			j = prng::rand64();
+}
