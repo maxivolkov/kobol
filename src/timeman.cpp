e06b59b19d39c98afe69680310eebf2ec605cd60
Maksim <maksim.volkov@mail.com>
2024-01-22 21:23:05 +0100

    Update timeman.cpp

--- src/timeman.cpp
+++ src/timeman.cpp
@@ -1,1 +1,15 @@
+#include <cstdint>
+#include "input.h"
+#include "search.h"
+#include "uci.h"
 #include "timeman.h"
+
+bool check_time()
+{
+	if (std::string input; get_input(input))
+		uci_command(input);
+	if (sp.game_over)
+		return true;
+	sp.game_over = sp.flags & tf_movetime && sd.elapsed() > static_cast<uint64_t>(sp.movetime);
+	return sp.game_over;
+}
